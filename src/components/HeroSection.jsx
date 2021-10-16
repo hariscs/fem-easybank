f447524107a053f4d26a58ff816305382740aaa8
Haris Shah <harisshah385@gmail.com>
2021-10-16 20:48:33 +0500

    hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,20 @@
+import classes from './HeroSection.module.css';
+import heroImg from '../images/image-mockups.png';
+
 const HeroSection = () => {
 	return (
-		<div>
-			<h1>Hero section</h1>
-		</div>
+		<section className={classes.hero}>
+			<div className={classes.hero__intro}>
+				<h1 className={classes.hero__title}>Next generation digital banking</h1>
+				<p className={classes.hero__subtitle}>
+					Take your financial life online. Your Easybank account will be a
+					one-stop-shop for spending, saving, budgeting, investing, and much
+					more.
+				</p>
+				<button>Request Invite</button>
+			</div>
+			<img src={heroImg} alt='hero' className={classes.hero__img} />
+		</section>
 	);
 };
 
