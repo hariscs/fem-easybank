679ebd99bc59811be9dc12e8e235cfd5c52a8efd
Haris Shah <harisshah385@gmail.com>
2021-10-18 12:14:37 +0500

    reuseable button component and navbar bug fixed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,24 @@
 import classes from './HeroSection.module.css';
 import heroImg from '../images/image-mockups.png';
+import Button from './Button';
 
 const HeroSection = () => {
 	return (
 		<section className={classes.hero}>
-			<div className={classes.hero__intro}>
-				<h1 className={classes.hero__title}>Next generation digital banking</h1>
-				<p className={classes.hero__subtitle}>
-					Take your financial life online. Your Easybank account will be a
-					one-stop-shop for spending, saving, budgeting, investing, and much
-					more.
-				</p>
-				<button>Request Invite</button>
+			<div className={classes.hero__container}>
+				<div className={classes.hero__intro}>
+					<h1 className={classes.hero__title}>
+						Next generation digital banking
+					</h1>
+					<p className={classes.hero__subtitle}>
+						Take your financial life online. Your Easybank account will be a
+						one-stop-shop for spending, saving, budgeting, investing, and much
+						more.
+					</p>
+					<Button>Request Invite</Button>
+				</div>
+				<img src={heroImg} alt='hero' className={classes.hero__img} />
 			</div>
-			<img src={heroImg} alt='hero' className={classes.hero__img} />
 		</section>
 	);
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Navbar.css';
 import logo from '../images/logo.svg';
 import hamburger from '../images/icon-hamburger.svg';
 import close from '../images/icon-close.svg';
+import Button from './Button';
 
 const Navbar = () => {
 	const [click, setClick] = useState(false);
+	const [button, setButton] = useState(true);
+
 	const handleClick = () => setClick(!click);
+
+	const showButton = () => {
+		if (window.innerWidth <= 960) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
+
+	useEffect(() => {
+		showButton();
+	}, []);
+
+	window.addEventListener('resize', showButton);
 
 	return (
 		<nav className='navbar'>
@@ -51,9 +68,11 @@ const Navbar = () => {
 						</a>
 					</li>
 				</ul>
-				<a href='/' className='btn cta'>
-					Request Invite
-				</a>
+				{button && (
+					<Button href='/' className='btn cta'>
+						Request Invite
+					</Button>
+				)}
 			</div>
 		</nav>
 	);
