a5faf98550a39ffc95f65106c874d7c98a19630c
Haris Shah <harisshah385@gmail.com>
2021-10-18 21:34:00 +0500

    article component

--- src/components/ArticleItem.jsx
+++ src/components/ArticleItem.jsx
@@ -1,12 +1,14 @@
+import classes from './ArticleItem.module.css';
+
 const ArticleItem = (props) => {
 	return (
 		<>
-			<li className='article__item'>
-				<img src={props.src} alt='article' className='article__img' />
-				<div className='article__text'>
-					<span className='article__author'>{props.author}</span>
-					<h3 className='article__title'>{props.title}</h3>
-					<p className='article__subtitle'>{props.subtitle}</p>
+			<li className={classes.article__item}>
+				<img src={props.src} alt='article' className={classes.article__img} />
+				<div className={classes.article__text}>
+					<span className={classes.article__author}>{props.author}</span>
+					<h3 className={classes.article__title}>{props.title}</h3>
+					<p className={classes.article__subtitle}>{props.subtitle}</p>
 				</div>
 			</li>
 		</>
