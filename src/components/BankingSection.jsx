e442846613a307ca024d6e413e70ea3d73967c0b
Haris Shah <harisshah385@gmail.com>
2021-10-18 14:27:33 +0500

    banking section completed

--- src/components/BankingSection.jsx
+++ src/components/BankingSection.jsx
@@ -8,36 +8,40 @@ import api from '../images/icon-api.svg';
 const BankingSection = () => {
 	return (
 		<section className={classes.bankingSection}>
-			<div className={classes.bankingSection__intro}>
-				<h2 className={classes.bankingSection__title}>Why choose EasyBank?</h2>
-				<p className={classes.bankingSection__subtitle}>
-					We leverage Open Banking to turn your bank account into your financial
-					hub. Control your finances like never before.
-				</p>
-			</div>
-			<div className='bankingSection__cardsContainer'>
-				<ul>
-					<CardItem
-						src={online}
-						title='Online Banking'
-						subTitle='Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.'
-					/>
-					<CardItem
-						src={simple}
-						title='Simple Budgeting'
-						subTitle='See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.'
-					/>
-					<CardItem
-						src={fast}
-						title='Fast Onboarding'
-						subTitle='We don’t do branches. Open your account in minutes online and start taking control of your finances right away.'
-					/>
-					<CardItem
-						src={api}
-						title='Open API'
-						subTitle='Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.'
-					/>
-				</ul>
+			<div className={classes.bankingContainer}>
+				<div className={classes.bankingSection__intro}>
+					<h2 className={classes.bankingSection__title}>
+						Why choose EasyBank?
+					</h2>
+					<p className={classes.bankingSection__subtitle}>
+						We leverage Open Banking to turn your bank account into your
+						financial hub. Control your finances like never before.
+					</p>
+				</div>
+				<div className={classes.bankingSection__cardsContainer}>
+					<ul className={classes.bankingSection__list}>
+						<CardItem
+							src={online}
+							title='Online Banking'
+							subTitle='Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.'
+						/>
+						<CardItem
+							src={simple}
+							title='Simple Budgeting'
+							subTitle='See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.'
+						/>
+						<CardItem
+							src={fast}
+							title='Fast Onboarding'
+							subTitle='We don’t do branches. Open your account in minutes online and start taking control of your finances right away.'
+						/>
+						<CardItem
+							src={api}
+							title='Open API'
+							subTitle='Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.'
+						/>
+					</ul>
+				</div>
 			</div>
 		</section>
 	);
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -5,8 +5,8 @@ function CardItem(props) {
 		<>
 			<li className={classes.card__item}>
 				<img src={props.src} alt='icon' />
-				<h3 className='card__itemTitle'>{props.title}</h3>
-				<p className='card__itemSubtitle'>{props.subTitle}</p>
+				<h3 className={classes.card__itemTitle}>{props.title}</h3>
+				<p className={classes.card__itemSubtitle}>{props.subTitle}</p>
 			</li>
 		</>
 	);
